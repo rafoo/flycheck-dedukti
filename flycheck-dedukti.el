ee06f37beef5a7bfb64f9e12e2887227bb550fc8
Raphaël Cauderlier <cauderlier@crans.org>
2014-10-01 18:22:56 +0200

    Define the checker appropriately at top-level.
    The checker now needs to be setup by
    
    (eval-after-load 'flycheck '(require flycheck-dedukti))
    (add-hook 'dedukti-mode-hook 'flycheck-mode)

--- flycheck-dedukti.el
+++ flycheck-dedukti.el
@@ -14,50 +14,43 @@
 ;; <https://www.rocq.inria.fr/deducteam/Dedukti/>
 ;; Flycheck is an on-the-fly syntax checker for GNU Emacs 24
 
-;; Package-Requires: ((f "0.11.0") (flycheck "0.19") (dedukti-mode "0.1"))
-
-;(dash "2.4.0")  (s "1.7.0") (pkg-info "0.4") (cl-lib "0.3") (emacs "24.1"))
+;; Package-Requires: ((flycheck "0.19") (dedukti-mode "0.1"))
 
 ;;; Code:
 
 (require 'flycheck)
 (require 'dedukti-mode)
 
-;;;###autoload
-(add-hook 'flycheck-mode-hook
-          (lambda ()
-            (flycheck-define-checker dedukti
-              "Dedukti type checker."
-              :command ("dkcheck"
-                        (eval dedukti-check-options)
-                        source-inplace)
-              :error-patterns
-              ((warning
-                line-start
-                "WARNING file:" (file-name)
-                " line:" line
-                " column:" column
-                (message) line-end)
-               (error
-                line-start
-                "ERROR file:" (file-name)
-                " line:" line
-                " column:" column
-                (message) line-end)
-               (warning
-                line-start
-                "WARNING line:" line
-                " column:" column
-                (message) line-end)
-               (error
-                line-start
-                "ERROR line:" line
-                " column:" column
-                (message) line-end))
-              :modes dedukti-mode)
-            (add-to-list 'flycheck-checkers 'dedukti)))
+(flycheck-define-checker dedukti
+  "Dedukti type checker."
+  :command ("dkcheck"
+            (eval dedukti-check-options)
+            source-inplace)
+  :error-patterns
+  ((warning
+    line-start
+    "WARNING file:" (file-name)
+    " line:" line
+    " column:" column
+    (message) line-end)
+   (error
+    line-start
+    "ERROR file:" (file-name)
+    " line:" line
+    " column:" column
+    (message) line-end)
+   (warning
+    line-start
+    "WARNING line:" line
+    " column:" column
+    (message) line-end)
+   (error
+    line-start
+    "ERROR line:" line
+    " column:" column
+    (message) line-end))
+  :modes dedukti-mode)
 
-;;;###autoload
-(add-hook 'dedukti-mode-hook 'flycheck-mode)
+(add-to-list 'flycheck-checkers 'dedukti)
 
 ;;; flycheck-dedukti.el ends here
